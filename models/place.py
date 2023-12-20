b68d3e18cc0ad8db83bec02963e02d27b2f7b314
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-20 16:47:32 +0300

    add setter method

--- models/place.py
+++ models/place.py
@@ -33,8 +33,8 @@ class Place(BaseModel, Base):
                 "amenity_id", String(60), ForeignKey("amenities.id"),
                 primary_key=True, nullable=False)
             )
-    amenities = relationship(
-            "Amenity", secondary="place_amenity", viewonly=False)
+    # amenities = relationship(
+    #         "Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
 
     @property
@@ -54,26 +54,32 @@ class Place(BaseModel, Base):
             if value.place_id == place_id:
                 review_list.append(value)
         return review_list
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place
+            """
+            from models import storage
+            from models.amenity import Amenity
+            amenity_list = []
+            for value in storage.all(Amenity).values():
+                if value.id in self.amenity_ids:
+                    amenity_list.append(value)
+            return amenity_list
 
-    @property
-    def amenities_getter(self):
-        """returns the list of Amenity instances based on the attribute
-        amenity_ids that contains all Amenity.id linked to the Place
-        """
-        amenity_list = []
-        for value in storage.all(Amentity).values():
-            if value.id in self.amenity_ids:
-                amenity_list.append(value)
-        return amenity_list
+        @amenities.setter
+        def amenities_s(self, value):
+            """Setter attribute amenities that handles append method
+                for adding an Amenity.id to the attribute amenity_ids
 
-    def amenities_s(self, value):
-        """Setter attribute amenities that handles append method
-            for adding an Amenity.id to the attribute amenity_ids
+                Note: This method should accept only Amenity object,
+                otherwise, do nothing.
+            """
+            from models.amenity import Amenity
 
-            Note: This method should accept only Amenity object,
-            otherwise, do nothing.
-        """
-        from models.amenity import Amenity
-
-        if isinstance(value, Amenity):
-            self.amenity_ids.append(value.id)
+            if isinstance(value, Amenity):
+                self.amenity_ids.append(value.id)
+    else:
+        amenities = relationship(
+            "Amenity", secondary="place_amenity", viewonly=False)
