b856e67394d82980f2d6351e405a57946e626147
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-11 22:37:02 +0000

    deploy a static website with puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,70 @@
+# sets up your web servers for the deployment of web_static with puppet
+
+#ensure nginx is installed
+package { 'nginx':
+  ensure	=> installed,
+}
+
+# ensure that the following directories exists
+file { '/data/':
+  ensure	=> 'directory',
+}
+file { '/data/web_static/':
+  ensure        => 'directory',
+}
+file { '/data/web_static/releases/':
+  ensure        => 'directory',
+}
+file { '/data/web_static/shared/':
+  ensure        => 'directory',
+}
+file { '/data/web_static/releases/test/':
+  ensure        => 'directory',
+}
+
+# create a fake html file with a little content
+file { '/data/web_static/releases/test/index.html':
+  ensure        => present,
+  content	=> 'Holberton School',
+}
+
+# create a symbolic link
+file { '/data/web_static/current':
+  ensure	=> 'link',
+  target	=> '/data/web_static/releases/test/',
+}
+
+# change owner and group of a directory to ubuntu
+file { '/data/':
+  ensure	=> 'directory',
+  owner		=> 'ubuntu',
+  group		=> 'ubuntu',
+}
+
+# Save the content of the server block in a variable
+$serverblock="server {
+        listen 80;
+        listen [::]:80;
+
+        root /data/web_static/;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name agroelectronics.tech;
+
+        location /hbnb_static {
+                alias /data/web_static/current/;
+        }
+}"
+
+# configure the server block to serve the content of /data/web_static/current/ to hbnb_static
+file { '/etc/nginx/sites-available/default':
+  ensure	=> present,
+  content	=> $serverblock
+}
+
+# Restart the nginx server
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
