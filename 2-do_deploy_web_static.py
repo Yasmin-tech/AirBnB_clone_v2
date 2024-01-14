17c38e235ca4f6584d028ae6882977d09d37701d
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-01-15 00:54:29 +0300

    add task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,55 @@
 #!/usr/bin/python3
-"""This module contains a Fabric script (based on the file
-    1-pack_web_static.py) that distributes an archive to
-    your web servers, using the function do_deploy.
-    """
-
-from fabric.api import run, put, env
-import datetime
+""" a Fabric script that distributes an archive
+    to my web servers, using the function do_deploy """
+from fabric.api import run, env, put
 import os
 
-env.user = "ubuntu"
-env.hosts = ['100.25.205.228', '100.25.211.79']
 
-
-def do_pack():
-    """ Create a archive from the content of a directory
-        and return the path to the archive
-    """
-    NOW = datetime.datetime.now()
-    y = NOW.year
-    mon = NOW.month
-    d = NOW.day
-    hr = NOW.hour
-    m = NOW.minute
-    sec = NOW.second
-
-    archv_name = 'web_static_{:02d}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz'.format(
-            y, mon, d, hr, m, sec)
-    local('mkdir -p versions')
-    local(f'tar -czvf versions/{archv_name} web_static/')
-    return f'versions/{archv_name}'
+env.hosts = ['ubuntu@100.25.151.250', 'ubuntu@35.174.211.176']
+env.sudo_user = "root"
+env.sudo_password = "12345sw48o"
 
 
 def do_deploy(archive_path):
-    """uncompress an archive file into a remote server directory
-    """
-
-    if not os.path.exists(archive_path):
+    """ the function that will run remotly ont the servers
+        to deploy the archived files """
+    if os.path.isfile(archive_path) is False:
         return False
-    put(archive_path, "/tmp/")
-    archive_name = os.path.basename(archive_path)
-    arch_filename = os.path.splitext(archive_name)[0]
-    arch_path = f'/tmp/{archive_name}'
-    run(f'mkdir -p /data/web_static/releases/{arch_filename}')
-    cmd = 'tar -xzf {} -C /data/web_static/releases/{} --strip-components=1'
-    run(cmd.format(arch_path, arch_filename))
-    run(f'rm {arch_path}')
-    run('rm -f /data/web_static/current')
-    s = f'ln -s /data/web_static/releases/{arch_filename} /data/web_static/current'
-    run(s)
-    print('New version deployed!')
+
+    if put(archive_path, remote_path="/tmp/").failed is True:
+        return False
+
+    paths = archive_path.split("/")
+    # the file without .tgz
+    paths = paths[-1].split(".")
+
+    file_to_uncompress = "/tmp/" + paths[0] + ".tgz"
+    if run("sudo mkdir -p /data/web_static/releases/{}/".format(
+            paths[0])).failed is True:
+        return False
+
+    new_dir = "/data/web_static/releases/{}/".format(paths[0])
+    if run("sudo tar -xzf {} -C {}".format(
+            file_to_uncompress, new_dir)).failed is True:
+        return False
+
+    if run("sudo rm {}".format(file_to_uncompress)).failed is True:
+        return False
+
+    if run("sudo mv {}web_static/* {}".format(
+            new_dir, new_dir)).failed is True:
+        return False
+
+    if run("sudo rm -rf {}web_static".format(new_dir)).failed is True:
+        return False
+
+    if run("sudo rm -rf /data/web_static/current").failed is True:
+        return False
+
+    if run("sudo ln -s {} /data/web_static/current".format(
+            new_dir)).failed is True:
+        return False
+
+    print("New version deployed!")
+
     return True
